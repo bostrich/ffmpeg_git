0dd569cbe6416e2f0730732c975d81a2309c0583
bostrich <13057506020@163.com>
2017-10-20 16:24:44 +0800

    commit  test

--- app/src/main/java/com/syezon/ffmpeg/MainActivity.java
+++ app/src/main/java/com/syezon/ffmpeg/MainActivity.java
@@ -1,24 +1,34 @@
 package com.syezon.ffmpeg;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
 
     // Used to load the 'native-lib' library on application startup.
     static {
+
+        System.loadLibrary("x264");
+        System.loadLibrary("avutil");
+        System.loadLibrary("avcodec");
+        System.loadLibrary("swresample");
         System.loadLibrary("native-lib");
-        System.loadLibrary("libavcodec");
-        System.loadLibrary("libavdevice");
-        System.loadLibrary("libavfilter");
-        System.loadLibrary("libavformat");
-        System.loadLibrary("libavutil");
-        System.loadLibrary("libpostproc");
-        System.loadLibrary("libswresample");
-        System.loadLibrary("libswscale");
-        System.loadLibrary("libx264");
+        System.loadLibrary("avdevice");
+        System.loadLibrary("avfilter");
+        System.loadLibrary("avformat");
+        System.loadLibrary("postproc");
+
+        System.loadLibrary("swscale");
+
     }
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +36,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        String abis = Build.CPU_ABI;
+        Log.e(TAG, abis);
+        tv = (TextView) findViewById(R.id.sample_text);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(stringFromJNI());
+            }
+        });
+
     }
 
     /**
